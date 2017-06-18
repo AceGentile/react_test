4c443ba99af3ae6c6ae064c0389f5230c4942fc1
crono <crono@loki>
2017-06-18 23:36:17 +0200

    hot reload

---
+++ client/components/Greetings.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <h1>this is react Hiii</h1>
+  );
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+    plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js'
+    ]
+  }
+}
