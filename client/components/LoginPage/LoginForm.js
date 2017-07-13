faa112710d6e114bf4922c89056d87245e9d9d2a
AceGentile <weatherbeaten@gmail.com>
2017-07-14 00:20:11 +0200

    login form

---
+++ client/components/LoginPage/LoginForm.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import TextFieldGroup from '../TextFieldGroup';
+import { validateInput } from '../../../server/shared/validations/login';
+import { login } from '../../actions/login';
+
+
+class LoginForm extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        identifier: '',
+        password: '',
+        errors: {},
+        isLoading: false,
+      }
+
+      this.onSubmit = this.onSubmit.bind(this);
+      this.onChange = this.onChange.bind(this);
+    }
+
+    isValid() {
+      const { isValid, errors } = validateInput(this.state);
+      if (!isValid) {
+        this.setState({ errors });
+      }
+      return isValid;
+    }
+
+    onSubmit(e) {
+      e.preventDefault();
+      if (this.isValid()) {
+        this.setState({
+          errors: {},
+          isLoading: true
+        });
+        this.props.login(this.state).then(
+          (res) => this.context.router.push('/'),
+          (err) => this.setState({ errors: err.response.data.errors, isLoading: false })
+        );
+      }
+    }
+
+    onChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+
+    render() {
+      const { errors, identifier, password, isLoading } = this.state;
+
+      return (
+        <form onSubmit={this.onSubmit}>
+          <h1>Login</h1>
+
+          <TextFieldGroup
+            field="identifier"
+            label="Username / Email"
+            value={identifier}
+            error={errors.identifier}
+            onChange={this.onChange}
+          />
+
+          <TextFieldGroup
+            field="password"
+            label="Password"
+            type="password"
+            value={identifier}
+            error={errors.password}
+            onChange={this.onChange}
+          />
+
+          <div className="form-group">
+            <button className="btn btn-primary btn-lg" disabled={isLoading}>Login</button>
+          </div>
+        </form>
+      );
+    }
+}
+
+LoginForm.propTypes = {
+  login: React.PropTypes.func.isRequired,
+}
+
+LoginForm.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+}
+
+export default connect(null, {login})(LoginForm);
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -12,6 +12,7 @@ export default () => {
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav navbar-right">
             <Link to="/signup" className="navbar-brand">Signup</Link>
+            <Link to="/login" className="navbar-brand">Login</Link>
           </ul>
         </div>
       </div>
